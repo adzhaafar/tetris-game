f798ab5165fae07c794e83c9029cf144c1ba9498
Alina Dzhaafar <dzhaafar.alina@gmail.com>
2022-02-08 23:39:54 +0300

    The tetraminos can be randomly drawn

--- app/script.js
+++ app/script.js
@@ -8,29 +8,67 @@ document.addEventListener('DOMContentLoaded', () => {
         gridSquares.classList.add('grid-item')
         gridSquares.innerHTML = i;
     }
-    //grabbing stuff
+    //grabbing html stuff 
     const rowIndex = 15;
     const columnIndex = 1;
     let squares = Array.from(document.querySelectorAll('.grid-item'));
     const scoreDisplay = document.querySelector('#score');
     const startBtn = document.querySelector('#start-btn');
-    //the tetrominoes
+   
+    // Tetrominos
 
-    // the L-shaped tetromino (an array with 4 arrays inside it, each array is a version of rotation of the piece)
-    //each of the small indices represents a cube on the grid
+    //each of the small indices represents a cube on the grid (an array with 4 arrays inside it, each array is a version of rotation of the piece)
+
+    // The L tetromino
     const lTetromino = [
         [2, 1, 16, 31],
         [1, 2, 3, 18],
         [1, 16, 31, 30],
         [15, 16, 17, 2],
-    ]
-    let currentIndex = 4;
-    function draw(shape) {
-        shape.forEach(
-            squares[currentIndex + shape].classList.add('tetromino'))
+    ];
+    // The Z tetromino
+    const zTetromino = [
+        [1, 2, 17, 18],
+        [1, 16, 17, 32], 
+        [15, 16, 1, 2],
+        [2, 17, 16, 31],
+        
+    ];
+    // The T tetromino
+    const tTetromino = [
+        [1, 2, 3, 17],
+        [1, 16, 31, 15],
+        [15, 16, 17, 1], 
+        [1, 16, 31, 17],
+
+    ];
+
+    //only 1 rotation because it is a block
+    const oTetromino = [
+        [1, 2, 16, 17],
+    ];
+
+    //only 2 rotations because it is a line
+    const iTetromino = [
+        [1, 16, 31, 46], 
+        [1, 2, 3, 4],
+    ];
+
+    //Array of tetrominos
+    const tetraminos = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+    let currentIndex = 0;
+    let whichShape = tetraminos[Math.floor(Math.random() * tetraminos.length)]
+    let tetraminoRotation = whichShape[Math.floor(Math.random() * whichShape.length)]
+    function draw() {
+        tetraminoRotation.forEach((index) => {
+            squares[currentIndex + index].classList.add('tetromino');
+
+        });
+      
     };
     
-draw(lTetromino[0])
+draw()
 
 //make all tetraminos and put them into array
 

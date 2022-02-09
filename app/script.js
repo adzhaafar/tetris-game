2c7f6717f59078455fc288dcf6799971454b4473
Alina Dzhaafar <dzhaafar.alina@gmail.com>
2022-02-09 10:04:25 +0300

    Tetramino moves down

--- app/script.js
+++ app/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-   //generate grid (15x20 squares)
+    //generate grid (15x20 squares)
     const grid = document.querySelector('.grid-container');
     for (let i = 0; i < 300; i++) {
         const gridSquares = document.createElement("div");
@@ -8,16 +8,20 @@ document.addEventListener('DOMContentLoaded', () => {
         gridSquares.classList.add('grid-item')
         gridSquares.innerHTML = i;
     }
+    // generate invisible helper grids
+    for (let i = 0; i < 15; i++) {
+        const gridSquaresEnd = document.createElement("div");
+        grid.appendChild(gridSquaresEnd);
+        gridSquaresEnd.classList.add('grid-taken');
+    }
+
+
     //grabbing html stuff 
-    const rowIndex = 15;
-    const columnIndex = 1;
     let squares = Array.from(document.querySelectorAll('.grid-item'));
     const scoreDisplay = document.querySelector('#score');
     const startBtn = document.querySelector('#start-btn');
    
-    // Tetrominos
-
-    //each of the small indices represents a cube on the grid (an array with 4 arrays inside it, each array is a version of rotation of the piece)
+    // Tetrominos (each of the small indices represents a cube on the grid (an array with 4 arrays inside it, each array is a version of rotation of the piece))
 
     // The L tetromino
     const lTetromino = [
@@ -29,76 +33,78 @@ document.addEventListener('DOMContentLoaded', () => {
     // The Z tetromino
     const zTetromino = [
         [1, 2, 17, 18],
-        [1, 16, 17, 32], 
+        [1, 16, 17, 32],
         [15, 16, 1, 2],
         [2, 17, 16, 31],
-        
     ];
     // The T tetromino
     const tTetromino = [
         [1, 2, 3, 17],
         [1, 16, 31, 15],
-        [15, 16, 17, 1], 
+        [15, 16, 17, 1],
         [1, 16, 31, 17],
-
     ];
-
     //only 1 rotation because it is a block
     const oTetromino = [
         [1, 2, 16, 17],
     ];
-
     //only 2 rotations because it is a line
     const iTetromino = [
-        [1, 16, 31, 46], 
+        [1, 16, 31, 46],
         [1, 2, 3, 4],
     ];
 
     //Array of tetrominos
     const tetraminos = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
 
+    // variables for the functions
     let currentIndex = 0;
-    let whichShape = tetraminos[Math.floor(Math.random() * tetraminos.length)]
-    let tetraminoRotation = whichShape[Math.floor(Math.random() * whichShape.length)]
-    function draw() {
-        tetraminoRotation.forEach((index) => {
-            squares[currentIndex + index].classList.add('tetromino');
+    let whichTetramino = tetraminos[Math.floor(Math.random() * tetraminos.length)];
+    let tetraminoRotation = whichTetramino[Math.floor(Math.random() * whichTetramino.length)];
 
+
+    //set timer (move piece down every second)
+    const timerId = setInterval(moveDown, 1000);
+
+    // move tetraminos down on the grid
+    function moveDown() {
+        undraw();
+        currentIndex += 15;
+        // freeze();
+        draw();
+    }
+
+
+    // The draw function
+    function draw() {
+        tetraminoRotation.forEach(index => {
+            squares[currentIndex + index].classList.add('tetromino');
         });
       
     };
+
+    // The undraw function
+    function undraw() {
+        tetraminoRotation.forEach(index => {
+            squares[currentIndex + index].classList.remove('tetromino')
+        });
+    };
+
+
+
+    //make tetrominos stop at the bottom of the page
     
-draw()
+    function freeze() {
+        if (tetraminoRotation.some((index) => {
+            squares[index + 15].classList.contains('grid-taken');
+        })) {
+            tetraminoRotation.forEach((index) => {
+                squares[index].classList.add('grid-freeze')
+                console.log(squares[index].classList)
+            })
+        }
+           
 
-//make all tetraminos and put them into array
-
-    
-    // the squares are an array of the little cube pieces, each having an index, by doing squares[currentposition + index] you will access a certain cube, the indexes come from the tetromino shapes which are coded. Then to that certain cube you add a class which will colour it a different color, so the shape will show up. 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    };
 
 });
